eef765e16459d740cd10679e51510795cc0ae14e
Daniel Kawakami <daniel.kawakami.en@gmail.com>
2020-09-21 19:03:32 -0300

    Aula 72 - Definindo rotas em arquivos separados

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const connection = require('./database/database')
 const categoriesController = require('./categories/CategoriesController')
+const articlesController = require('./articles/ArticlesController')
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: false}))
@@ -16,6 +17,10 @@ connection
     }).catch((error)=>{
         console.log(error)
     })
+
+app.use('/', categoriesController)
+
+app.use('/', articlesController)
 
 app.get("/", (req, res)=>{
     res.render("index");
